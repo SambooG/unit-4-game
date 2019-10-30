f89fb0d3a7f47b8a2da99e22cf2bb6bb5094424b
Samantha Gaiser <sgaiser3@gmail.com>
2019-10-30 14:34:33 -0400

    adding pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+// <!-- create 4 buttons with id and class -->
+/* !-- generate a random number for the score between 19-120 (math.random math.floor?-->
+
+    <!-- generate a random hidden number for 1-12 points for each gem
+
+    <!-- create variable for wins and losses -->
+
+    listen for a click?
+
+    
+    is the total of the value clicked less than the target score?
+        if yes keep adding to the scored points
+        store new value
+
+        else if equal to the variable set for the random number then 
+            alert you win
+            add a win
+            reset to 0 points
+            generate new random number 
+
+
+            else if it greater than random number you lost
+
+            add a loss to counter
+
+            
+     begin a new game
+            reset score to 0 
+            pick a new random number for the target 
+            set new values for each gem stone
+
+    // run the random number generator */
